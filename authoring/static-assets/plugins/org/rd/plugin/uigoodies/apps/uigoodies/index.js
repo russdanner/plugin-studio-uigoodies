149a5b83711635b80dba4c32d8ab95e7f9fced37
russdanner <russ.danner@craftercms.com>
2023-11-01 11:05:27 -0400

    update binaries

--- authoring/static-assets/plugins/org/rd/plugin/uigoodies/apps/uigoodies/index.js
+++ authoring/static-assets/plugins/org/rd/plugin/uigoodies/apps/uigoodies/index.js
@@ -719,13 +719,13 @@ function OpenContentUploadToolbarButton(props) {
 }
 
 function PullPushRemoteButtons(props) {
-    var useIcon = props.useIcon, remoteName = props.remoteName, mergeStrategy = props.mergeStrategy, pullBranch = props.pullBranch, pushBranch = props.pushBranch, pullLabel = props.pullLabel, pushLabel = props.pushLabel;
+    var useIcon = props.useIcon, remoteName = props.remoteName, mergeStrategy = props.mergeStrategy, pullBranch = props.pullBranch, pushBranch = props.pushBranch, pullLabel = props.pullLabel, pushLabel = props.pushLabel, _a = props.enablePull, enablePull = _a === void 0 ? true : _a, _b = props.enablePush, enablePush = _b === void 0 ? true : _b;
     var siteId = useActiveSiteId();
     useEnv();
-    var _a = React.useState(''), snackMessage = _a[0], setSnackMessage = _a[1];
-    var _b = React.useState(true), snackSuccess = _b[0], setSnackSuccess = _b[1];
-    var _c = React.useState(false), snackShow = _c[0], setSnackShow = _c[1];
-    var _d = React.useState(false), progressShow = _d[0], setProgressShow = _d[1];
+    var _c = React.useState(''), snackMessage = _c[0], setSnackMessage = _c[1];
+    var _d = React.useState(true), snackSuccess = _d[0], setSnackSuccess = _d[1];
+    var _e = React.useState(false), snackShow = _e[0], setSnackShow = _e[1];
+    var _f = React.useState(false), progressShow = _f[0], setProgressShow = _f[1];
     var onPullSuccess = function (result) {
         setSnackMessage("".concat(pullLabel ? pullLabel : 'Pull', " completed successfully."));
         setSnackSuccess(true);
@@ -779,19 +779,16 @@ function PullPushRemoteButtons(props) {
         setProgressShow(false);
         setSnackShow(false);
     }
-    return useIcon ? (React.createElement(React.Fragment, null,
-        React.createElement(Tooltip, { title: pullLabel ? pullLabel : "Pull" },
-            React.createElement(IconButton, { size: "small", onClick: handlePullClick },
-                React.createElement(DownloadRoundedIcon, null))),
-        React.createElement(Tooltip, { title: pushLabel ? pushLabel : "Push" },
-            React.createElement(IconButton, { size: "small", onClick: handlePushClick },
-                React.createElement(PublishRoundedIcon, null))),
-        React.createElement(Backdrop, { sx: { color: '#fff', zIndex: function (theme) { return theme.zIndex.drawer + 1; } }, open: progressShow },
-            React.createElement(CircularProgress$1, { color: "inherit" }),
-            React.createElement(Snackbar, { anchorOrigin: { vertical: 'top', horizontal: 'center' }, open: snackShow, autoHideDuration: 5000, onClose: handleSnackClose },
-                React.createElement(Alert, { severity: snackSuccess ? "success" : "error", sx: { width: '100%' } }, snackMessage))))) : (React.createElement(React.Fragment, null,
-        React.createElement(Button, { size: "small", variant: "text", onClick: handlePullClick }, pullLabel ? pullLabel : "Pull"),
-        React.createElement(Button, { size: "small", variant: "text", onClick: handlePushClick }, pushLabel ? pushLabel : "Push"),
+    return (React.createElement(React.Fragment, null,
+        useIcon ? (React.createElement(React.Fragment, null,
+            enablePull ? (React.createElement(Tooltip, { title: pullLabel ? pullLabel : "Pull" },
+                React.createElement(IconButton, { size: "small", onClick: handlePullClick },
+                    React.createElement(DownloadRoundedIcon, null)))) : (React.createElement(React.Fragment, null)),
+            enablePush ? (React.createElement(Tooltip, { title: pushLabel ? pushLabel : "Push" },
+                React.createElement(IconButton, { size: "small", onClick: handlePushClick },
+                    React.createElement(PublishRoundedIcon, null)))) : (React.createElement(React.Fragment, null)))) : (React.createElement(React.Fragment, null,
+            enablePull ? (React.createElement(Button, { size: "small", variant: "text", onClick: handlePullClick }, pullLabel ? pullLabel : "Pull")) : (React.createElement(React.Fragment, null)),
+            enablePush ? (React.createElement(Button, { size: "small", variant: "text", onClick: handlePushClick }, pushLabel ? pushLabel : "Push")) : (React.createElement(React.Fragment, null)))),
         React.createElement(Backdrop, { sx: { color: '#fff', zIndex: function (theme) { return theme.zIndex.drawer + 1; } }, open: progressShow },
             React.createElement(CircularProgress$1, { color: "inherit" }),
             React.createElement(Snackbar, { anchorOrigin: { vertical: 'top', horizontal: 'center' }, open: snackShow, autoHideDuration: 5000, onClose: handleSnackClose },
